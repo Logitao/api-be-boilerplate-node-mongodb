bc22443f1b9c661afe0519e8964069af2e5945ab
Marcelo Fortunato <marcelo.fortunato@totvs.com.br>
2018-04-16 14:53:28 -0300

    Implements JWT

--- src/modules/token/controllers.ts
+++ src/modules/token/controllers.ts
@@ -1,10 +1,10 @@
 import { Request, Response, Application } from 'express';
 
-import Token from './services';
-import db from '../../models/config.models';
-import Handlers from '../../utils/handlers';
-import userModel, { IUserModel, IUserAttributes } from '../../models/user.model';
+import { IUserModel } from '../../models/user.model';
 
+import db from '../../models/config.models';
+import Token from './services';
+import Handlers from '../../utils/handlers';
 
 class TokenController {
 
@@ -13,13 +13,20 @@ class TokenController {
     async createToken(req: Request, res: Response) {
         const errorMessage: string = 'Unathorized, wrong email or password!';                        
         try {
-            const user: IUserModel = await db.User.findOne({email: req.body.email});              
-            if (!user || !await new db.User().isPassword(req.body.password, user.password)) {
-                throw new Error(errorMessage);
-            }                        
+
+            const user: IUserModel = await db.User.findOne({email: req.body.email});    
+            const isPassword: boolean = await new db.User().isPassword(req.body.password, user.password);
+            
+            if (!user || !isPassword) throw new Error(errorMessage);
+
             Handlers.onSuccess(res, await Token.create(req.body));
-        } catch (error) {                       
-            Handlers.onError(res, error.message, error);
+        } catch (error) {  
+            console.log(error.message); 
+            if (error.message !== errorMessage) {
+                Handlers.onError(res, error.message, error);    
+            } else {
+                Handlers.onError(res, `Error CREATE Token`, error);
+            }
         }    
     }
 }
